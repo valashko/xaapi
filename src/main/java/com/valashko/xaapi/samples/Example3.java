8ee2a5117f2f5417d52e6fc3d2a454638f3b7b59
Ilya Lysenko <lysenko.ilya@gmail.com>
2019-12-18 23:51:43 +0300

    Minor fixes

--- src/main/java/com/valashko/xaapi/samples/Example3.java
+++ src/main/java/com/valashko/xaapi/samples/Example3.java
@@ -1,23 +1,21 @@
 package com.valashko.xaapi.samples;
 
-import com.valashko.xaapi.XaapiException;
 import com.valashko.xaapi.device.XiaomiGateway;
 import com.valashko.xaapi.device.XiaomiGatewayLight;
 
 import java.awt.*;
-import java.io.IOException;
 
 /**
  * Police lights
  *
- * You can find a gateway password here:
+ * You can find the gateway password here:
  * Mi Home App > Select the Gateway device > ... (dots at the top right of the screen) > About > Wireless communication protocol.
- * If the "Wireless communication protocol" option isn't shown, tap a plugin version number at the bottom of the screen several time.
+ * If the "Wireless communication protocol" option isn't shown, tap a plugin version number at the bottom of the screen several times.
  */
 public class Example3 {
     private static final int SLEEP_TIMEOUT = 150;
 
-    public static void main(String[] args) throws IOException, XaapiException, InterruptedException {
+    public static void main(String[] args) throws Exception {
         String password = "a22b4b5b6c7cc0d";
         XiaomiGateway gateway = new XiaomiGateway("192.168.1.123", password);
         XiaomiGatewayLight gatewayLight = gateway.getBuiltinLight();
